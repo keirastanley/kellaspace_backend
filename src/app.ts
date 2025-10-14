e917b11dcc78717519e59e824ce005ed7ede7b27
keirastanley <keiraestanley@gmail.com>
2025-10-14 19:18:31 +0100

    feat: create express app

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { MongoClient } from "mongodb";
+import { getRecommendationsRouter } from "./routes/recommendations.router";
+import { tmdbRouter } from "./routes/tmdb.router";
+
+dotenv.config();
+
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+
+app.use(cors());
+
+MongoClient.connect(process.env.MONGO_URI!)
+  .then((client) => {
+    const db = client.db();
+    app.use("/api/recommendations", getRecommendationsRouter(db));
+    app.use("/api/tmdb", tmdbRouter);
+
+    app.listen(port, () => {
+      console.log(`Listening on port ${port}`);
+    });
+  })
+  .catch((err) => console.error(err));
